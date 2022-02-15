d678d1bc938dfe2868df924c2665b9825a0ffd4f
anna-vaughn <60945664+anna-vaughn@users.noreply.github.com>
2022-02-15 17:59:43 -0600

    Update settings.py

--- localLibrary/settings.py
+++ localLibrary/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0gqsgizqws_)-#-mm+3!diloymci!ca-ptzh$t7$^u(t$dr@rt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
